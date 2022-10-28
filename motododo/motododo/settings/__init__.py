0132cf21386b5d8d6e896aec00a7128896c4b9f6
Fabe59 <fabrice.pringuet@free.fr>
2022-10-28 16:39:16 +0200

    yaml to deploy on azure

--- motododo/motododo/settings/__init__.py
+++ motododo/motododo/settings/__init__.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ') 
