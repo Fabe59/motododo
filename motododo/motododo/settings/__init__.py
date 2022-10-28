a167154b81b010a14c7e0607b53453bf8a21ea8c
Fabe59 <fabrice.pringuet@free.fr>
2022-10-28 17:44:25 +0200

    settings updated to deploy on azure

--- motododo/motododo/settings/__init__.py
+++ motododo/motododo/settings/__init__.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = os.environ('ALLOWED_HOSTS')
 
 # Application definition
 
