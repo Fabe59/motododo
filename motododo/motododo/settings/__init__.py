86bb8d68593dd5d320f0de1c88f650f7c141f505
Fabe59 <fabrice.pringuet@free.fr>
2022-10-28 18:06:32 +0200

    settings updated to deploy on azure

--- motododo/motododo/settings/__init__.py
+++ motododo/motododo/settings/__init__.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'motododo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DBNAME"),
-        'USER': os.getenv("DBUSER"),
-        'PASSWORD': os.getenv("DBPASSWD"),
-        'HOST': os.getenv("DBHOST"),
+        'NAME': str(os.getenv("DBNAME")),
+        'USER': str(os.getenv("DBUSER")),
+        'PASSWORD': str(os.getenv("DBPASSWD")),
+        'HOST': str(os.getenv("DBHOST")),
         'PORT': '5432',
     }
 }
@@ -141,11 +142,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
 
-GOOGLE_RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHAPASSWD")
+GOOGLE_RECAPTCHA_SECRET_KEY = str(os.getenv("RECAPTCHAPASSWD"))
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv("MAILLOGIN")
-EMAIL_HOST_PASSWORD = os.getenv("MAILPASSWD")
+EMAIL_HOST_USER = str(os.getenv("MAILLOGIN"))
+EMAIL_HOST_PASSWORD = str(os.getenv("MAILPASSWD"))
